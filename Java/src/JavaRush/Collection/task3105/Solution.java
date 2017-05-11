22894fe3fba644fa88225e904982f0d53b7217a4
BogdanMykhalchhuk <bogden1979@yahoo.com>
2017-05-11 19:37:59 +0300

    10 05

--- Java/src/JavaRush/Collection/task3105/Solution.java
+++ Java/src/JavaRush/Collection/task3105/Solution.java
@@ -1,11 +1,51 @@
 package JavaRush.Collection.task3105;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /* 
 Добавление файла в архив
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+//        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(args[1]));
+//        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(args[0]));
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream("D:\\Test\\Test.zip"));
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream("D:\\Test\\Test.zip"));
+        Map<ZipEntry, byte[]> map = new HashMap<>();
+        ZipEntry nextZipEntry;
+        while((nextZipEntry = zipIn.getNextEntry()) != null) {
+            if (!nextZipEntry.isDirectory()) {
+                byte[] bytes = new byte[(int) nextZipEntry.getSize()];
+                zipIn.read(bytes);
+                map.put(nextZipEntry, bytes);
+                zipIn.closeEntry();
+            } else {
+                map.put(nextZipEntry, null);
+            }
+        }
+
+        zipIn.close();
+
+        for(ZipEntry zipEntry : map.keySet()) {
+            zipOut.putNextEntry(zipEntry);
+            if(!zipEntry.isDirectory()) {
+                zipOut.write(map.get(zipEntry));
+            }
+            zipOut.closeEntry();
+        }
+        ZipEntry zipEntry = new ZipEntry("new\\Test1.txt");
+        zipOut.putNextEntry(zipEntry);
+        File file = new File("D:\\Test\\Test.txt");
+        Files.copy(file.toPath(), zipOut);
+        zipOut.close();
     }
 }
--- Java/src/Test/TestTest.java
+++ Java/src/Test/TestTest.java
@@ -1,38 +1,14 @@
 package Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public final class TestTest {
 
     public static void main(String[] args) {
-        int[][] tiles = new int[4][4];
-
-        int count = 0;
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                tiles[i][j] = ++count;
-            }
-        }
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                System.out.print(tiles[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        int[][] temp = new int[4][4];
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                temp[j][4 - 1 - i] = tiles[i][j];
-            }
-        }
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
-                System.out.print(temp[i][j] + " ");
-            }
-            System.out.println();
-        }
+        Path root = Paths.get("D:\\Test\\");
+        Path path = Paths.get("D:\\Test\\Temp\\create\\yandex19-logo-ru.png");
+        System.out.println(root.relativize(path));
     }
 }
 
