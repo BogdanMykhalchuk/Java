3d893ae4d4f25035411d1ff6644fb4f6e576ab70
BogdanMykhalchuk <bogden1979@yahoo.com>
2017-05-16 17:01:47 +0300

    16 05

--- Java/src/JavaRush/Collection/task3206/Solution.java
+++ Java/src/JavaRush/Collection/task3206/Solution.java
@@ -1,5 +1,7 @@
 package JavaRush.Collection.task3206;
 
+import java.lang.reflect.Proxy;
+
 /*
 Дженерики для создания прокси-объекта
 */
@@ -7,11 +9,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        test(solution.getProxy(Item.class));                        //true false false
-//        test(solution.getProxy(Item.class, Small.class));           //true false true
-//        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
-//        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
-//        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+        test(solution.getProxy(Item.class));                        //true false false
+        test(solution.getProxy(Item.class, Small.class));           //true false true
+        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
     }
 
 
@@ -21,5 +23,13 @@ public class Solution {
         boolean isSmall = proxy instanceof Small;
 
         System.out.format("%b %b %b\n", isItem, isBig, isSmall);
+    }
+
+    public <T extends Item> T getProxy(Class<? extends Item> object, Class<?>... interfaces) {
+        ClassLoader classLoader = object.getClassLoader();
+        Class[] extendedClasses = new Class[interfaces.length + 1];
+        System.arraycopy(interfaces, 0, extendedClasses, 0, interfaces.length);
+        extendedClasses[extendedClasses.length - 1] = object;
+        return (T)Proxy.newProxyInstance(classLoader, extendedClasses, new ItemInvocationHandler());
     }
 }
